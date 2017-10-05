eab420eafa8b2a8d33d8c36145ac5336d5e57ee6
Arif Hossain <arifthecoder@gmail.com>
2017-10-05 21:57:41 +0300

    introduction of REST API
    rest framework

---
+++ restAPIintroduction/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url
+from django.contrib import admin
+from currency import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^stocks/', views.StockList.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
